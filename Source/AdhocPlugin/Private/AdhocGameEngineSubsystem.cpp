816dd39a678319b81ce7f9c4fc00c21a7e0d49e9
SpeculativeCoder <116658408+SpeculativeCoder@users.noreply.github.com>
2025-01-01 18:24:02 +0000

    Update copyright

--- Source/AdhocPlugin/Private/AdhocGameEngineSubsystem.cpp
+++ Source/AdhocPlugin/Private/AdhocGameEngineSubsystem.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2022-2024 SpeculativeCoder (https://github.com/SpeculativeCoder)
+﻿// Copyright (c) 2022-2025 SpeculativeCoder (https://github.com/SpeculativeCoder)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
